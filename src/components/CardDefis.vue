0209cdecf82dd1a0db37b1e2a397f3853f90b76b
maugan-robert <maugan.robert@edu.univ-fcomte.fr>
2024-06-09 18:37:09 +0200

    retoouche carddefis

--- src/components/CardDefis.vue
+++ src/components/CardDefis.vue
@@ -13,17 +13,17 @@ import IconTime from './icons/IconTime.vue'
             <p class="text-sm font-manrope font-semibold text-teal-600">Course à pied</p>
             <div class="flex items-center gap-1 ">
             <div class="flex items-center">
-                <p class="text-base font-semibold text-zinc-900">600</p>
-                <IconMoney />
+                <p class="text-sm font-lexend font-medium text-zinc-900">600</p>
+                <IconMoney class="" />
             </div>
             <div class="flex items-center">
-                <p class="text-base font-semibold text-zinc-900">1200</p>
+                <p class="text-sm font-lexend font-medium text-zinc-900">1200</p>
                 <IconMoneyPremium />
             </div>
             </div>
             <div class="bg-zinc-200 rounded-lg flex items-center justify-center w-24 px-1">
-                <IconTime class="w-5 h-5"/>
-                <p class="p-1 text-sm font-medium text-zinc-900 ">3j et 6h</p>
+                <IconTime class="opacity-90 w-5 h-5"/>
+                <p class="p-1 text-sm font-manrope text-zinc-900 ">3j et 6h</p>
             </div>
         </div>
         <IconFav class="fill-transparent" />
